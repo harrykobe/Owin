6531e8a63dd23d6c48f39c9c47872b14b224c13a
Harry Yung <harrykobe@gmail.com>
2014-12-17 16:52:46 +0800

    第一次提交操作

---
+++ TestOwin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace TestOwin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://*:9007";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                //var response = client.GetAsync(baseAddress + "/api/values").Result;
+
+                //Console.WriteLine(response);
+                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.WriteLine("Server running on {0}", baseAddress);
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ TestOwin/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+namespace TestOwin
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            appBuilder.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration{};
+                map.RunSignalR(hubConfiguration);
+            });
+
+            string exeFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string webFolder = Path.Combine(exeFolder, "Web");
+            appBuilder.UseStaticFiles(new Microsoft.Owin.StaticFiles.StaticFileOptions()
+            {
+                RequestPath = new PathString(""),
+                FileSystem = new PhysicalFileSystem(webFolder)
+            });
+
+
+            appBuilder.UseCors(CorsOptions.AllowAll);
+            appBuilder.MapSignalR();
+            appBuilder.UseWebApi(config);
+
+        }
+    }
+
+    public class MyHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+    }
+}
