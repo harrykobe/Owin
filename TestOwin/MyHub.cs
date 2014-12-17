30ded60d258b29c81694ece09c4447ba7ee40033
容均致 <harrykobe@gmail.com>
2014-12-17 18:37:10 +0800

    整合SignalR Hub功能

---
+++ TestOwin/MyHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNet.SignalR;
+
+namespace TestOwin
+{
+    public class MyHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+    }
+}
--- TestOwin/Startup.cs
+++ TestOwin/Startup.cs
@@ -51,12 +51,4 @@ namespace TestOwin
 
         }
     }
-
-    public class MyHub : Hub
-    {
-        public void Send(string message)
-        {
-            Clients.All.addMessage(message);
-        }
-    }
 }
