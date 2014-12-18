d219b1ae1766dc7832ab713627c633a92e407657
容均致 <harrykobe@gmail.com>
2014-12-18 16:53:15 +0800

    测试指定用户发送信息

--- TestOwin/Hubs/MyHub.cs
+++ TestOwin/Hubs/MyHub.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 
 namespace TestOwin.Hubs
 {
@@ -8,17 +11,45 @@ namespace TestOwin.Hubs
         private readonly static ConnectionMapping<string> _connections =
             new ConnectionMapping<string>();
 
-        //public override Task OnConnected()
-        //{
-        //    string name = Context.User.Identity.Name;
+        public override Task OnConnected()
+        {
+            //string name = Context.User.Identity.Name;
+            int n = new Random().Next(1000) + 1;
+            string name = Convert.ToString(n);
+            Console.WriteLine("{0} is Connect...", name);
+            _connections.Add(name, Context.ConnectionId);
+            return base.OnConnected();
+        }
 
-        //    _connections.Add(name, Context.ConnectionId);
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.User.Identity.Name;
+            _connections.Remove(name, Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
 
-        //    return base.OnConnected();
-        //}
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+            return base.OnReconnected();
+        }
+
         public void Send(string message)
         {
-            Clients.All.addMessage(message); //
+            Clients.All.addMessage(message);
+        }
+
+        public void SendChatMessage(string who, string message)
+        {
+            //string name = Context.User.Identity.Name;
+            foreach (var connectionId in _connections.GetConnections(who))
+            {
+                Clients.Client(connectionId).addMessage(message);
+            }
         }
     }
 }
