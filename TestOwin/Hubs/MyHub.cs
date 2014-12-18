bd6263e0ed65af135659449e139e990ba13d9e04
容均致 <harrykobe@gmail.com>
2014-12-18 13:19:54 +0800

    测试Hub连接事件功能

--- TestOwin/Hubs/MyHub.cs
+++ TestOwin/Hubs/MyHub.cs
@@ -1,9 +1,21 @@
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
-namespace TestOwin
+namespace TestOwin.Hubs
 {
     public class MyHub : Hub
     {
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+
+        //public override Task OnConnected()
+        //{
+        //    string name = Context.User.Identity.Name;
+
+        //    _connections.Add(name, Context.ConnectionId);
+
+        //    return base.OnConnected();
+        //}
         public void Send(string message)
         {
             Clients.All.addMessage(message);
