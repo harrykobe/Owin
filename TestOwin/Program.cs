ebff65d8eb588da15f8589a4b6df06581ab0a382
Harry Rong <harrykobe@gmail.com>
2015-01-10 09:04:02 +0800

    增加防火墙添加删除可信程序以及端口功能

--- TestOwin/Program.cs
+++ TestOwin/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Hosting;
+using TestOwin.Helper;
 
 namespace TestOwin
 {
@@ -9,10 +10,12 @@ namespace TestOwin
         static void Main(string[] args)
         {
             string baseAddress = "http://*:9007";
+            INetFwManger.NetFwGetPorts(33065, "TCP");
             using (WebApp.Start<Startup>(url: baseAddress))
             {
                 Console.WriteLine("Server running on {0}", baseAddress);
                 Console.ReadLine();
+                //INetFwManger.NetFwDelPorts(9007, "TCP");
             }
 
             
