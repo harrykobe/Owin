65ce90ac63893ab43d9740a13f728c6343b56eea
Harry Rong <harrykobe@gmail.com>
2015-02-14 13:33:15 +0800

    精简文件

--- TestOwin/Startup.cs
+++ TestOwin/Startup.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
